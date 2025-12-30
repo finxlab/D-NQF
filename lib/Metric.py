9b60bfdbb3b7d83eed793fe98014b9f1032d38c6
jys-ai <65596272+jys-ai@users.noreply.github.com>
2025-12-30 19:52:42 +0900

    Add files via upload

--- lib/Metric.py
+++ lib/Metric.py
@@ -5,22 +5,24 @@ import numpy as np
 
 
 def ND(prediction_samples, labels):
-    median_pred = np.quantile(prediction_samples, 0.5, axis=-1, keepdims = True)
+    # median_pred = np.quantile(prediction_samples, 0.5, axis=-1, keepdims = True)
+    index = int(200 * 0.5 - 1)
+    median_pred = prediction_samples[:, :, index:index+1]
     diff = np.sum(np.abs(median_pred - labels))
     summation = np.sum(np.abs(labels))
-    # print("diff", median_pred.shape, labels.shape, (median_pred - labels).shape)
     return diff, summation
 
 def NRMSE(prediction_samples, labels):
     N, T, _ = labels.shape
-    median_pred = np.quantile(prediction_samples, 0.5, axis=-1, keepdims = True)
+    # median_pred = np.quantile(prediction_samples, 0.5, axis=-1, keepdims = True)
+    index = int(200 * 0.5 - 1)
+    median_pred = prediction_samples[:, :, index:index+1]
     diff = np.sum((median_pred - labels) ** 2)
     summation = np.sum(np.abs(labels))
     # print(diff.shape)
     return diff, summation, N * T
 
 def CRPS(prediction_samples, labels):
-    # ensemble = np.sort(prediction_samples, axis=-1)
     N, T, M = prediction_samples.shape
     N, T, _ = labels.shape
     alpha_array = np.concatenate([np.arange(1/M, 0.999, 1 / M), np.array([0.999])])[np.newaxis, np.newaxis, :]
@@ -28,7 +30,8 @@ def CRPS(prediction_samples, labels):
     return np.sum(pinball_loss), (N * T * M)
 
 def QLOSS(prediction_samples, labels, q=0.5):
-    quantile = np.quantile(prediction_samples, q, axis=-1, keepdims = True)
+    index = int(200 * q - 1)
+    quantile = prediction_samples[:, :, index:index+1]
     N, T, _ = labels.shape
     qloss = (labels - quantile) * (q - (labels <= quantile).astype(float))
     return np.sum(qloss), N * T
@@ -45,35 +48,48 @@ def ECRPS(prediction_samples, labels):
 
 def QCL(prediction_samples, labels):
     N, T, M = prediction_samples.shape
+    
     lower = prediction_samples[:, :, :-1]
     upper = prediction_samples[:, :, 1:]
     diff = lower - upper
-    zeros = np.zeros_like(diff)
-    relu_diff = np.maximum(zeros, diff)
+    relu_diff = (diff > 0).astype(int)
     loss = np.sum(relu_diff)
     return loss, N * T
 
 def MSE(prediction_samples, labels):
     N, T,_ = labels.shape
-    median_pred = np.quantile(prediction_samples, 0.5, axis=-1, keepdims = True)
+    # median_pred = np.quantile(prediction_samples, 0.5, axis=-1, keepdims = True)
+    index = int(200 * 0.5 - 1)
+    median_pred = prediction_samples[:, :, index:index+1]
     return np.sum((median_pred - labels) ** 2), N * T
 
 def Rsquare(prediction_samples, labels): # XXXX not appropriate
     N, T,_ = labels.shape
-    median_pred = np.quantile(prediction_samples, 0.5, axis=-1, keepdims = True)
+    # median_pred = np.quantile(prediction_samples, 0.5, axis=-1, keepdims = True)
+    index = int(200 * 0.5 - 1)
+    median_pred = prediction_samples[:, :, index:index+1]
     return np.sum((median_pred - labels) ** 2) / np.sum((np.mean(labels) - labels)**2), 1
 
 def MAE(prediction_samples, labels):
     N, T,_ = labels.shape
-    median_pred = np.quantile(prediction_samples, 0.5, axis=-1, keepdims = True)
+    # median_pred = np.quantile(prediction_samples, 0.5, axis=-1, keepdims = True)
+    
+    index = int(200 * 0.5 - 1)
+    median_pred = prediction_samples[:, :, index:index+1]
     return np.sum(np.abs(median_pred - labels)), N * T
 
 def MAPE(prediction_samples, labels):
     N, T,_ = labels.shape
-    median_pred = np.quantile(prediction_samples, 0.5, axis=-1, keepdims = True)
+    # median_pred = np.quantile(prediction_samples, 0.5, axis=-1, keepdims = True)
+    
+    index = int(200 * 0.5 - 1)
+    median_pred = prediction_samples[:, :, index:index+1]
     return np.sum(np.abs((median_pred - labels) / labels)), N * T
 
 def MSPE(prediction_samples, labels):
     N, T,_ = labels.shape
-    median_pred = np.quantile(prediction_samples, 0.5, axis=-1, keepdims = True)
+    # median_pred = np.quantile(prediction_samples, 0.5, axis=-1, keepdims = True)
+    
+    index = int(200 * 0.5 - 1)
+    median_pred = prediction_samples[:, :, index:index+1]
     return np.sum(((median_pred - labels) / labels) ** 2), N * T
